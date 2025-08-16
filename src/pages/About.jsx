04cce26daf35a8acfd00ab0c09e5226d20477638
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-08-16 12:43:39 +0100

    fix: Update text formatting in Layout4 and adjust layout styles in Layout6 for consistency; enhance About page structure and content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,53 +1,69 @@
-// import image from '../assets/765aef0e597f450af751170944ee1ba2dfd5296a.jpg';
+import { useNavigate } from 'react-router-dom';
+import image from '../assets/765aef0e597f450af751170944ee1ba2dfd5296a.jpg';
+import { useContext } from 'react';
+import { ModalContext } from '../components/store/ModalContext';
 
-// export default function About(){
-//     return(
-//         <div className='lg:w-[1375px] h-auto lg:h-[1016px] pl-[32px] grid gap-[32px] '>
+export default function About(){
+    const modalCtx = useContext(ModalContext);
+    const navigate = useNavigate();
 
-//             <div className='lg:w-[1375px] h-auto lg:h-[181px] p-[10px] grid gap-[10px] '>
-//                 <h1 className='font-[700] font-[Poppins] text-[39.81px] '>About Us</h1>
+    function handleOpenModal(){
+        modalCtx.showModal('signup')
+    }
 
-//                 <p className='font-[400] font-[Poppins] text-[19.2px] '>Founded in 2025, ClimateSmart Agro was created to support farmers facing the growing challenges of climate change. Our mission is simple: help farmers grow more, lose less, and farm smarter, no matter the weather. <br />We combine local wisdom with digital tools to provide climate-resilient solutions right at the farmer is fingertips.</p>
-//             </div>
+    function handleClick(){
+        navigate('/')
+    }
+    return(
+        <div className='lg:w-[1400px] h-auto lg:h-[1016px] p-[32px] lg:grid lg:gap-[32px] '>
 
-//             <div className='lg:w-[1375px] h-auto lg:h-[687px] flex   gap-[32px] '>
+            <div className='lg:w-[1330px] h-auto lg:h-[181px] p-[10px] lg:grid lg:gap-[10px] '>
+                <h1 className='font-[700] font-[Poppins] text-[39.81px] '>About Us</h1>
 
-//                 <div className='lg:w-[769px] h-auto lg:h-[687px] grid-[8px] '>
-//                     <h2>What We Offer</h2>
-//                     <p>
-//                         <li>Localized weather & pest alerts to help you prepare and protect your crops.</li>
+                <p className='font-[400] font-[Poppins] text-[19.2px] '>Founded in 2025, ClimateSmart Agro was created to support farmers facing the growing challenges of climate change. Our mission is simple: help farmers grow more, lose less, and farm smarter, no matter the weather. <br />We combine local wisdom with digital tools to provide climate-resilient solutions right at the farmer is fingertips.</p>
+            </div>
 
-//                         <li>Expert farming advice in clear, local languages.</li>
+            <div className='lg:w-[1330px] h-auto lg:h-[687px] lg:flex lg:gap-[32px]  '>
 
-//                         <li>Climate-smart practices like soil conservation, crop rotation, and agroforestry.</li>
+                <div className='lg:w-[769px] h-auto lg:h-[687px] flex flex-col gap-[15px]'>
+                    <h2 className='font-[Poppins] font-[400] text-[27.65px] pt-5 lg:pt-0'>What We Offer</h2>
+                    <p className='font-[Poppins] font-[400] text-[19.2px] grid gap-[27px]'>
+                        <li>Localized weather & pest alerts to help you prepare and protect your crops.</li>
 
-//                         <li>Export Hub to guide farmers into global markets.</li>
+                        <li>Expert farming advice in clear, local languages.</li>
 
-//                         <li>Resilient farming tools and techniques tailored to your region.</li>
+                        <li>Climate-smart practices like soil conservation, crop rotation, and agroforestry.</li>
 
-//                         <li>Smart Farming drop-down, with educational content on soil care, crop planning, and more.</li>
+                        <li>Export Hub to guide farmers into global markets.</li>
 
-//                         <li>Training materials and real-life success stories from farmers like you.</li>
+                        <li>Resilient farming tools and techniques tailored to your region.</li>
 
-//                         <li>Community features to connect, learn, and grow with other farmers.</li>
+                        <li>Smart Farming drop-down, with educational content on soil care, crop planning, and more.</li>
 
-//                         At ClimateSmart Agro, we believe the future of farming is smart, sustainable, and farmer-led.
+                        <li>Training materials and real-life success stories from farmers like you.</li>
 
-//                         Lets grow together.
-//                     </p>
-//                 </div>
+                        <li>Community features to connect, learn, and grow with other farmers.</li>
+   
+                    </p>
+                    <p className='font-[Poppins] font-[400] text-[19.2px] pt-6'>At ClimateSmart Agro, we believe the future of farming is smart, sustainable, and farmer-led.</p>
+                    <p className='font-[Poppins] font-[400] text-[19.2px] pt-6 lg:pb-0 pb-6'> Lets grow together.</p>
+                </div>
 
-//                 <div className='lg:w-[574px] h-auto lg:h-[687px] grid gap-[64px] '>
+                <div className='lg:w-[500px] h-auto lg:h-[600px] flex flex-col gap-[64px]  '>
 
-//                     <img className='lg:w-[574px] lg:h-[578px] h-auto rounded-[20px] ' src={image} alt="soil measurement" />
+                    <img className='lg:w-[574px] lg:h-[500px] h-auto rounded-[20px] ' src={image} alt="soil measurement" />
 
-//                     <div className='lg:w-[574px] h-auto lg:h-[45px] flex gap-32px'>
-//                         <button className='lg:w-[271px] h-[45px] rounded-[8px] border-[1px] px-[16px] py-[8px] flex gap-10px '>Back</button>
-//                         <button className='lg:w-[271px] h-[45px] rounded-[8px] border-[1px] px-[16px] py-[8px] flex gap-10px bg-[#FF8E28]'>Lets Start</button>
-//                     </div>
-//                 </div>
-//             </div>
+                    <div className='lg:w-[500px] h-auto lg:h-[45px] font-[700] text-[19.2px] flex lg:gap-[32px] gap-[20px]'>
+                        <button
+                        onClick={handleClick}
+                        className='lg:w-[271px] h-[45px] w-[150px] rounded-[8px] border-[1px] border-[#FF8E28] hover:bg-[#b86014]  lg:px-[90px] px-13.5 py-[8px] flex gap-10px '>Back</button>
+                        <button
+                        onClick={handleOpenModal}
+                        className='lg:w-[271px] h-[45px] w-[150px]  rounded-[8px] border-[1px] px-7 lg:px-[60px] py-[8px] flex gap-10px border-[#FF8E28] hover:bg-[#b86014] bg-[#FF8E28]'>Lets Start</button>
+                    </div>
+                </div>
+            </div>
 
-//         </div>
-//     )
-// }
+        </div>
+    )
+}
