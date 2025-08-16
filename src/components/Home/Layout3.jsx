a21f58d1ef16b4833134b0d5f4ebfeea5c59e93e
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-08-15 18:09:45 -0700

    feat: Remove commented-out code in Layout3 component for cleaner implementation

--- src/components/Home/Layout3.jsx
+++ src/components/Home/Layout3.jsx
@@ -1,31 +1,31 @@
-// import image3 from '../../assets/f5fbc5a4f44543ea95fc553cdfce6679696440c3.jpg';
+import image3 from '../../assets/f5fbc5a4f44543ea95fc553cdfce6679696440c3.jpg';
 
-// export default function Layout3() {
-//     return(
-//         <div className='bg-[#FFFFFF] lg:w-[1440px] h-auto lg:h-[536.65px]  lg:p-[32px] flex flex-col justify-center items-center gap-[10px] px-[16px] py-[16px] j text-center'>  
+export default function Layout3() {
+    return(
+        <div className='bg-[#FFFFFF] lg:w-[1440px] h-auto lg:h-[536.65px]  lg:p-[32px] flex flex-col justify-center items-center gap-[10px] px-[16px] py-[16px] j text-center'>  
 
-//                     <div className=' lg:w-[1120px] h-auto lg:h-[472.65px] grid gap-[32px] '>
+                    <div className=' lg:w-[1120px] h-auto lg:h-[472.65px] grid gap-[32px] '>
 
-//                     <div className='flex flex-col h-auto lg:h-[215px] lg:gap-[32px] gap-[19px] '>
-//                         <h1 className='font-[Poppins] font-[600] text-[28px] lg:text-[39.81px] '>Our Smart Solution.</h1>
+                    <div className='flex flex-col h-auto lg:h-[215px] lg:gap-[32px] gap-[19px] '>
+                        <h1 className='font-[Poppins] font-[600] text-[28px] lg:text-[39.81px] '>Our Smart Solution.</h1>
         
-//                         <p className='lg:w-[1123px] font-[Poppins] font-[400] text-[20px] lg:text-[27.65px] text-center'>ClimateSmart Agro equips farmers with Localized weather alerts, Expert advice, <br /> and Climate-resilient farming tools. We are using tech to bridge the gap between <br />traditional farming and modern climate realities.
-//                         </p>
+                        <p className='lg:w-[1123px] font-[Poppins] font-[400] text-[20px] lg:text-[27.65px] text-center'>ClimateSmart Agro equips farmers with Localized weather alerts, Expert advice, <br /> and Climate-resilient farming tools. We are using tech to bridge the gap between <br />traditional farming and modern climate realities.
+                        </p>
         
-//                  </div>
+                 </div>
 
-//                         <div className='flex justify-center'>
-//                             <img 
-//                             className='lg:w-[263.68px] lg:h-[225.65px] rounded-tr-[16px] rounded-tl-[8px] rounded-br-[16px]
-//                             rounded-bl-[8px] w-[300px] ' 
-//                             src={image3} 
-//                             alt="A man in the desert" />
-//                             </div>
-//                         </div>
+                        <div className='flex justify-center'>
+                            <img 
+                            className='lg:w-[263.68px] lg:h-[225.65px] rounded-tr-[16px] rounded-tl-[8px] rounded-br-[16px]
+                            rounded-bl-[8px] w-[300px] ' 
+                            src={image3} 
+                            alt="A man in the desert" />
+                            </div>
+                        </div>
                         
         
                     
-//                  </div>        
-//     )
-// }
+                 </div>        
+    )
+}
 
