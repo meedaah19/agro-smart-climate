215fd13c4bf9e9f99fe62d787731034fd03917e3
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-08-15 18:10:11 -0700

    feat: Remove commented-out code in Layout4 component for cleaner implementation

--- src/components/Home/Layout4.jsx
+++ src/components/Home/Layout4.jsx
@@ -1,30 +1,30 @@
-// import image1 from '../../assets/6cfa44040bb6a722e442b44d773f0aa31fbd7b57.png'
+import image1 from '../../assets/6cfa44040bb6a722e442b44d773f0aa31fbd7b57.png'
 
 
-// export default function Layout3() {
-//     return(
-//         <div className='bg-gradient-to-b from-[#FFFFFF] via-[#C6E4E1] to-[#C6E4E1] lg:w-[1440px] h-auto lg:h-[666px]  lg:p-[32px] grid gap-[10px] px-[16px] py-[16px] text-center'>  
+export default function Layout3() {
+    return(
+        <div className='bg-gradient-to-b from-[#FFFFFF] via-[#C6E4E1] to-[#C6E4E1] lg:w-[1440px] h-auto lg:h-[666px]  lg:p-[32px] grid gap-[10px] px-[16px] py-[16px] text-center'>  
 
-//                     <div className=' lg:w-[1376px] h-auto lg:h-[565.86px] grid gap-[32px] '>
+                    <div className=' lg:w-[1376px] h-auto lg:h-[565.86px] grid gap-[32px] '>
 
-//                     <div className='flex flex-col h-auto lg:h-[215px] lg:gap-[32px] gap-[19px] justify-center items-center'>
-//                         <h1 className='font-[Poppins] font-[600] text-[28px] lg:text-[39.81px] '>Built for Every Farmer, Backed by Community.</h1>
+                    <div className='flex flex-col h-auto lg:h-[215px] lg:gap-[32px] gap-[19px] justify-center items-center'>
+                        <h1 className='font-[Poppins] font-[600] text-[28px] lg:text-[39.81px] '>Built for Every Farmer, Backed by Community.</h1>
         
-//                         <p className='lg:w-[1123px] font-[Poppins] font-[400] text-[20px] lg:text-[27.65px] text-center'>Whether you are a smallholder farmer or part of a rural community, it`s built for everyone. <br /> Our platform is designed to be accessible, user-friendly, and adaptable to the unique <br /> needs of farmers across Nigeria.
-//                         </p>
+                        <p className='lg:w-[1123px] font-[Poppins] font-[400] text-[20px] lg:text-[27.65px] text-center'>Whether you are a smallholder farmer or part of a rural community, it`s built for everyone. <br /> Our platform is designed to be accessible, user-friendly, and adaptable to the unique <br /> needs of farmers across Nigeria.
+                        </p>
         
-//                  </div>
+                 </div>
 
-//                         <div className='flex justify-center'>
-//                             <img 
-//                             className='lg:w-[389px] lg:h-[437px] rounded-[16px] w-[300px] ' 
-//                             src={image1} 
-//                             alt="A man in the desert" />
-//                             </div>
-//                         </div>
+                        <div className='flex justify-center'>
+                            <img 
+                            className='lg:w-[389px] lg:h-[437px] rounded-[16px] w-[300px] ' 
+                            src={image1} 
+                            alt="A man in the desert" />
+                            </div>
+                        </div>
                         
         
                     
-//                  </div>
-//     )
-// }
+                 </div>
+    )
+}
