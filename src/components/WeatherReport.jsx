039f1dcd18e9da3d01196d3d7739201ce4e90abd
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-08-21 14:10:39 +0100

    add

--- src/components/WeatherReport.jsx
+++ src/components/WeatherReport.jsx
@@ -43,7 +43,7 @@ export default function WeatherReport(){
 
 
     return(
-        <div className='lg:w-[1000px] h-auto lg:h-[536px]  flex flex-col gap-[8px]'>
+        <div className='lg:w-[1000px] h-auto lg:h-[536px]  flex flex-col gap-[25px]'>
 
             <div className="lg:w-[1030px] h-auto lg:h-[35px] w-[300px] flex justify-between gap-[10px] ">
                 <h1 className="font-[Poppins] text-[23.04px]">Weather report</h1>
