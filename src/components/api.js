945b4053e9b206cff7859d96b98897b9ecd51e62
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2025-09-16 15:36:22 +0100

    .

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 import {supabase} from "../supabaseClient";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000" ;
+const API_BASE_URL ="http://localhost:5000" || import.meta.env.VITE_API_URL  ;
 
 export function isTokenValid() {
   const token = localStorage.getItem("access_token");
